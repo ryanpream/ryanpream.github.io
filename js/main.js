5c64425eee73cd8719837ce6312a5d542758d6fa
ryanpream <ryan@ryanpream.com>
2015-05-12 13:13:12 -0500

    limited preloader II

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
+
+
 function main() {
 
 (function () {
    'use strict';
-
     //Page Loader
         //<![CDATA[
       $(window).load(function() { // makes sure the whole site is loaded
-        $('#status').fadeOut(); // will first fade out the loading animation
-        $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-        $('body').delay(350).css({'overflow':'visible'});
+        //$('#status').fadeOut(); // will first fade out the loading animation
+        //$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+        //$('body').delay(350).css({'overflow':'visible'});
       });
       //]]>
+
 
    // Contact form toggle hide/show
     $(document).ready(function() {
