c1650fe339073372755a56f0ecd26eb03c1a9bec
ryanpream <ryan@ryanpream.com>
2015-05-12 12:46:45 -0500

    attempt to add back preloader

--- js/main.js
+++ js/main.js
@@ -3,6 +3,15 @@ function main() {
 (function () {
    'use strict';
 
+    //Page Loader
+        //<![CDATA[
+      $(window).load(function() { // makes sure the whole site is loaded
+        $('#status').fadeOut(); // will first fade out the loading animation
+        $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+        $('body').delay(350).css({'overflow':'visible'});
+      });
+      //]]>
+
    // Contact form toggle hide/show
     $(document).ready(function() {
       $("#show").click(function() {
